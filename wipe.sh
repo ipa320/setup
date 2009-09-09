7f6b2a14806849c8c820899a21b05f19640c0f4d
tekkub <tekkub@gmail.com>
2009-09-08 21:25:32 -0700

    Backticks? I don't see any backticks

--- wipe.sh
+++ wipe.sh
@@ -3,7 +3,7 @@
 # Simple script to wipe the git config variables that GitHub depends on
 # Useful if the user wants to start from scratch
 
-`git config --global --unset user.name`
-`git config --global --unset user.email`
-`git config --global --unset github.user`
-`git config --global --unset github.token`
+git config --global --unset user.name
+git config --global --unset user.email
+git config --global --unset github.user
+git config --global --unset github.token

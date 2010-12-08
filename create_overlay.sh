748fa81383caa6677b6e1510e282b3744489b908
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2010-12-08 17:37:21 +0100

    overlay script

---
+++ create_overlay.sh
@@ -0,0 +1,149 @@
+#!/usr/bin/env bash
+
+# Basic script to set up the git config variables needed by GitHub.
+# After the user enters his user/password, everything should be pre-populated.
+# The user can, of course, choose to override the values the script digs up for user.name and user.email
+
+# checking input parameters
+# $1 -i for ipa setup (optional)
+if [ "$#" -ge 2 ]; then
+	echo "ERROR: Wrong number of parameters"
+	echo "Usage: create_overlay.sh STACK"
+	exit 1
+elif [ "$1" = "cob_extern" ]; then
+	STACK=$1
+elif [ "$1" = "cob_common" ]; then
+	STACK=$1
+elif [ "$1" = "cob_driver" ]; then
+	STACK=$1
+elif [ "$1" = "cob_simulation" ]; then
+	STACK=$1
+elif [ "$1" = "cob_apps" ]; then
+	STACK=$1
+else
+	echo "ERROR: stack <<"$1">> not supported, aborting..."
+	exit 1
+fi
+
+echo "-------------------------------------------"
+echo "Creating overlay for stack <<"$STACK">>"
+echo "-------------------------------------------"
+echo ""
+
+# Check git user name
+user=`git config --global github.user`
+token=`git config --global github.token`
+if [ -z "$user" ]; then
+	read -p "GitHub username not found, please enter: " -e user
+fi
+
+
+# Setup gh token
+if [ -z "$token" ]; then
+	echo "GitHub token not found in global git config"
+	read -s -p "Please enter GitHub password for $user (this will not be saved): " -e password
+	echo "" # Because we didn't echo the user's return key above
+
+	echo "Fetching API token"
+	acct=`curl https://github.com/account --user $user:$password 2> /dev/null`
+	token=`echo "$acct" | grep "API token" | sed 's/<p>.*<code>//' |sed 's/<\/code>.*//'`
+
+	if [ $token ]; then
+		#echo $token
+		echo "Saving GitHub token to global git config"
+		`git config --global github.user $user`
+		`git config --global github.token $token`
+	else
+		echo "Error retrieving token"
+		echo "Is curl installed? Please install curl with 'sudo apt-get install curl'."
+		exit 1
+	fi
+fi
+
+if [ -z "$acct" ]; then
+	echo "Fetching GitHub account details"
+	acct=`curl -F "login=$user" -F "token=$token" https://github.com/account 2> /dev/null`
+fi
+
+
+# Setup username
+gitname=`git config --global user.name`
+if [ -z "$gitname" ]; then
+	gitname=$user
+fi
+read -p "Enter git committer name (return to use '$gitname'): " -e newgitname
+
+if [ -z "$newgitname" ]; then
+	newgitname=$gitname
+fi
+`git config --global user.name "$newgitname"`
+
+
+# Setup email
+ghemail=`echo "$acct" | grep 'class="address"' | head -n1 | sed "s/.*>\([^@]*@[^<]*\)<.*/\1/"`
+gitemail=`git config --global user.email`
+if [ -z "$gitemail" ]; then
+	gitemail=$ghemail
+fi
+read -p "Enter git committer email (return to use '$gitemail'): " -e newgitemail
+
+if [ -z "$newgitemail" ]; then
+	newgitemail=$gitemail
+fi
+`git config --global user.email "$newgitemail"`
+
+# SSH keys!
+if [ ! -f ~/.ssh/id_rsa ]; then
+	read -p "No id_rsa key found, generate one? (y/N) "
+	echo ""
+	if [[ $REPLY = [yY] ]]; then
+		ssh-keygen -t rsa -f ~/.ssh/id_rsa -N "No passphrase"
+	fi
+fi
+
+if [ -f ~/.ssh/id_rsa ]; then
+	read -p "Upload id_rsa key to your GitHub account? (y/N) "
+	if [[ $REPLY = [yY] ]]; then
+		sshkey=`cat ~/.ssh/id_rsa.pub`
+		acct=`curl -F "login=$user" -F "token=$token" https://github.com/account/public_keys -F "public_key[title]=$USER@$HOSTNAME" -F "public_key[key]=$sshkey" 2> /dev/null`
+	fi
+	echo ""
+fi
+
+
+# Fork stack on github
+acct=`curl -F "login=$user" -F "token=$token" https://github.com/ipa320/$STACK/fork 2> /dev/null`
+
+
+# Clone stack
+mkdir -p ~/git
+mkdir -p ~/git/care-o-bot
+cd ~/git/care-o-bot && git clone git@github.com:$user/$STACK.git
+
+
+# Setup bashrc for ROS with cturtle and care-o-bot stacks. First delete all entries and then add them in the correct order.
+sed -i '/ros/ d' ~/.bashrc
+sed -i '/care-o-bot/ d' ~/.bashrc
+
+if [ -d /opt/ros/cturtle ]; then
+	`echo "source /opt/ros/cturtle/setup.sh" >> ~/.bashrc 2> /dev/null`
+	ROS_PACKAGE_PATH="/opt/ros/cturtle/stacks"
+else
+	print "ERROR: No ROS cturtle release found"
+fi
+
+if [ -d ~/ros ]; then
+	`rm ~/ros/setup.sh 2> /dev/null`
+	touch ~/ros/setup.sh
+	`echo "export ROS_PACKAGE_PATH=$HOME/ros:$ROS_PACKAGE_PATH" >> ~/ros/setup.sh 2> /dev/null`
+	`echo "source $HOME/ros/setup.sh" >> ~/.bashrc 2> /dev/null`
+fi
+
+if [ -d ~/git/care-o-bot ]; then
+	touch ~/git/care-o-bot/setup.sh
+	`echo "export ROS_PACKAGE_PATH=$HOME/git/care-o-bot:$ROS_PACKAGE_PATH" >> ~/git/care-o-bot/setup.sh 2> /dev/null`
+	`echo "COUNT=$(cat /proc/cpuinfo | grep 'processor' | wc -l)" >> ~/git/care-o-bot/setup.sh 2> /dev/null`
+	`echo "COUNT=$(echo "$COUNT*2" | bc)" >> ~/git/care-o-bot/setup.sh 2> /dev/null`
+	`echo "export ROS_PARALLEL_JOBS=-j$COUNT" >> ~/git/care-o-bot/setup.sh 2> /dev/null`
+	`echo "source $HOME/git/care-o-bot/setup.sh" >> ~/.bashrc 2> /dev/null`
+fi

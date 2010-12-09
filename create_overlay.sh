9567cf5c9e43895c5ef3e8844b6756508e88e2fa
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2010-12-09 11:29:17 +0100

    added anonymous checkout

--- create_overlay.sh
+++ create_overlay.sh
@@ -34,87 +34,90 @@ echo ""
 user=`git config --global github.user`
 token=`git config --global github.token`
 if [ -z "$user" ]; then
-	read -p "GitHub username not found, please enter: " -e user
+	read -p "GitHub username not found, please enter (leave blank for read-only access): " -e user
 fi
 
+readonly=false
+if [ -z "$user" ]; then
+	readonly=true
+else
+	# Setup gh token
+	if [ -z "$token" ]; then
+		echo "GitHub token not found in global git config"
+		read -s -p "Please enter GitHub password for $user (this will not be saved): " -e password
+		echo "" # Because we didn't echo the user's return key above
 
-# Setup gh token
-if [ -z "$token" ]; then
-	echo "GitHub token not found in global git config"
-	read -s -p "Please enter GitHub password for $user (this will not be saved): " -e password
-	echo "" # Because we didn't echo the user's return key above
+		echo "Fetching API token"
+		acct=`curl https://github.com/account --user $user:$password 2> /dev/null`
+		token=`echo "$acct" | grep "API token" | sed 's/<p>.*<code>//' |sed 's/<\/code>.*//'`
 
-	echo "Fetching API token"
-	acct=`curl https://github.com/account --user $user:$password 2> /dev/null`
-	token=`echo "$acct" | grep "API token" | sed 's/<p>.*<code>//' |sed 's/<\/code>.*//'`
-
-	if [ $token ]; then
-		#echo $token
-		echo "Saving GitHub token to global git config"
-		`git config --global github.user $user`
-		`git config --global github.token $token`
-	else
-		echo "Error retrieving token"
-		echo "Is curl installed? Please install curl with 'sudo apt-get install curl'."
-		exit 1
+		if [ $token ]; then
+			#echo $token
+			echo "Saving GitHub token to global git config"
+			`git config --global github.user $user`
+			`git config --global github.token $token`
+		else
+			echo "Error retrieving token"
+			echo "Is curl installed? Please install curl with 'sudo apt-get install curl'."
+			exit 1
+		fi
 	fi
-fi
 
-if [ -z "$acct" ]; then
-	echo "Fetching GitHub account details"
-	acct=`curl -F "login=$user" -F "token=$token" https://github.com/account 2> /dev/null`
-fi
-
-
-# Setup username
-gitname=`git config --global user.name`
-if [ -z "$gitname" ]; then
-	gitname=$user
-fi
-read -p "Enter git committer name (return to use '$gitname'): " -e newgitname
-
-if [ -z "$newgitname" ]; then
-	newgitname=$gitname
-fi
-`git config --global user.name "$newgitname"`
-
-
-# Setup email
-ghemail=`echo "$acct" | grep 'class="address"' | head -n1 | sed "s/.*>\([^@]*@[^<]*\)<.*/\1/"`
-gitemail=`git config --global user.email`
-if [ -z "$gitemail" ]; then
-	gitemail=$ghemail
-fi
-read -p "Enter git committer email (return to use '$gitemail'): " -e newgitemail
-
-if [ -z "$newgitemail" ]; then
-	newgitemail=$gitemail
-fi
-`git config --global user.email "$newgitemail"`
-
-# SSH keys!
-if [ ! -f ~/.ssh/id_rsa ]; then
-	read -p "No id_rsa key found, generate one? (y/N) "
-	echo ""
-	if [[ $REPLY = [yY] ]]; then
-		ssh-keygen -t rsa -f ~/.ssh/id_rsa
-		sleep 2
+	if [ -z "$acct" ]; then
+		echo "Fetching GitHub account details"
+		acct=`curl -F "login=$user" -F "token=$token" https://github.com/account 2> /dev/null`
 	fi
-fi
 
-if [ -f ~/.ssh/id_rsa ]; then
-	read -p "Upload id_rsa key to your GitHub account? (y/N) "
-	if [[ $REPLY = [yY] ]]; then
-		sshkey=`cat ~/.ssh/id_rsa.pub`
-		acct=`curl -F "login=$user" -F "token=$token" https://github.com/account/public_keys -F "public_key[title]=$USER@$HOSTNAME" -F "public_key[key]=$sshkey" 2> /dev/null`
+
+	# Setup username
+	gitname=`git config --global user.name`
+	if [ -z "$gitname" ]; then
+		gitname=$user
 	fi
-	echo ""
+	read -p "Enter git committer name (return to use '$gitname'): " -e newgitname
+
+	if [ -z "$newgitname" ]; then
+		newgitname=$gitname
+	fi
+	`git config --global user.name "$newgitname"`
+
+
+	# Setup email
+	ghemail=`echo "$acct" | grep 'class="address"' | head -n1 | sed "s/.*>\([^@]*@[^<]*\)<.*/\1/"`
+	gitemail=`git config --global user.email`
+	if [ -z "$gitemail" ]; then
+		gitemail=$ghemail
+	fi
+	read -p "Enter git committer email (return to use '$gitemail'): " -e newgitemail
+
+	if [ -z "$newgitemail" ]; then
+		newgitemail=$gitemail
+	fi
+	`git config --global user.email "$newgitemail"`
+
+	# SSH keys!
+	if [ ! -f ~/.ssh/id_rsa ]; then
+		read -p "No id_rsa key found, generate one? (y/N) "
+		echo ""
+		if [[ $REPLY = [yY] ]]; then
+			ssh-keygen -t rsa -f ~/.ssh/id_rsa
+			sleep 2
+		fi
+	fi
+
+	if [ -f ~/.ssh/id_rsa ]; then
+		read -p "Upload id_rsa key to your GitHub account? (y/N) "
+		if [[ $REPLY = [yY] ]]; then
+			sshkey=`cat ~/.ssh/id_rsa.pub`
+			acct=`curl -F "login=$user" -F "token=$token" https://github.com/account/public_keys -F "public_key[title]=$USER@$HOSTNAME" -F "public_key[key]=$sshkey" 2> /dev/null`
+		fi
+		echo ""
+	fi
+
+
+	# Fork stack on github
+	acct=`curl -F "login=$user" -F "token=$token" https://github.com/ipa320/$STACK/fork 2> /dev/null`
 fi
-
-
-# Fork stack on github
-acct=`curl -F "login=$user" -F "token=$token" https://github.com/ipa320/$STACK/fork 2> /dev/null`
-
 
 # Clone stack
 mkdir -p ~/git
@@ -127,7 +130,11 @@ if [ -d ~/git/care-o-bot/$STACK ]; then
 		cd ~/git/care-o-bot/$STACK && git pull origin master
 	fi
 else
-	cd ~/git/care-o-bot && git clone git@github.com:$user/$STACK.git
+	if $readonly; then
+		cd ~/git/care-o-bot && git clone git://github.com/ipa320/$STACK.git
+	else
+		cd ~/git/care-o-bot && git clone git@github.com:$user/$STACK.git
+	fi
 fi
 
 

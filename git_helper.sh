c4c95f5df8369a83f40f2f2fdca4fdab2ab5ecd5
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2010-12-22 11:15:07 +0100

    add git_helper script

---
+++ git_helper.sh
@@ -0,0 +1,55 @@
+#!/bin/bash
+
+# checking input parameters
+if [ "$#" -ge 2 ]; then
+	echo "ERROR: Wrong number of parameters"
+	echo "Usage: create_overlay.sh STACK"
+	exit 1
+elif [ "$1" = "status" ]; then
+	COMMAND="git status -uno"
+elif [ "$1" = "commit" ]; then
+	read -p "Commit message:"
+	COMMAND="git commit -a -m'$REPLY'"
+elif [ "$1" = "pull" ]; then
+	COMMAND="git pull"
+elif [ "$1" = "push" ]; then
+	COMMAND="git pull && git push"
+elif [ "$1" = "push320" ]; then
+	COMMAND="git checkout review-320 && git pull origin-320 master && git push origin-320 master && git checkout master"
+elif [ "$1" = "merge320" ]; then
+	COMMAND="git checkout review-320 && git pull origin-320 master && git checkout master && git merge review-320 && git push origin master"
+else
+	echo "ERROR: command <<"$1">> not supported, choose one of"
+	echo "    * status"
+	echo "    * commit"
+	echo "    * pull"
+	echo "    * push"
+	echo "    * merge320"
+	echo "aborting..."
+	exit 1
+fi
+
+echo "-------------------------------------------"
+echo "Executing <<$COMMAND>>"
+echo "-------------------------------------------"
+echo ""
+
+echo ""
+echo "==> operating on ~/git/care-o-bot/cob_extern"
+cd ~/git/care-o-bot/cob_extern ; sh -c "$COMMAND"
+
+echo ""
+echo "==> operating on ~/git/care-o-bot/cob_common"
+cd ~/git/care-o-bot/cob_common; sh -c "$COMMAND"
+
+echo ""
+echo "==> operating on ~/git/care-o-bot/cob_driver"
+cd ~/git/care-o-bot/cob_driver; sh -c "$COMMAND"
+
+echo ""
+echo "==> operating on ~/git/care-o-bot/cob_simulation"
+cd ~/git/care-o-bot/cob_simulation; sh -c "$COMMAND"
+
+echo ""
+echo "==> operating on ~/git/care-o-bot/cob_apps"
+cd ~/git/care-o-bot/cob_apps; sh -c "$COMMAND"

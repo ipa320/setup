71d67dae4fdfb2f63f9f6587adf2ffb71b519492
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2011-06-13 16:37:17 +0200

    pushing and pulling origin master

--- git_helper.sh
+++ git_helper.sh
@@ -13,9 +13,9 @@ elif [ "$1" = "commit" ]; then
 	read -p "Commit message:"
 	COMMAND="git commit -a -m'$REPLY'"
 elif [ "$1" = "pull" ]; then
-	COMMAND="git pull"
+	COMMAND="git pull origin master"
 elif [ "$1" = "push" ]; then
-	COMMAND="git pull && git push"
+	COMMAND="git pull origin master && git push origin master"
 elif [ "$1" = "push320" ]; then
 	COMMAND="git checkout review-320 && git pull origin-320 master && git push origin-320 master && git checkout master"
 elif [ "$1" = "merge320" ]; then
@@ -42,6 +42,7 @@ echo "-------------------------------------------"
 echo ""
 
 dirs=`ls ~/git/care-o-bot`
+mkdir -p ~/tmp
 echo "$dirs" >  ~/tmp/stacks.txt
 while read directory
 do

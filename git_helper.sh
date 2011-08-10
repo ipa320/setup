33bc5fa1e717d42a6a0782789c5af71497bb5288
ipa-fmw <mmb-weisshardtf@ipa.fhg.de>
2011-08-10 15:39:20 +0200

    combine git_helper with merge320

--- git_helper.sh
+++ git_helper.sh
@@ -1,9 +1,19 @@
 #!/bin/bash
 
+show_usage(){
+echo "Usage: git_helper.sh command [options]"
+echo "    * status              shows new and modified files"
+echo "    * master              switch to master branch"
+echo "    * commit              commits all new and modified file !!check with 'status' before commiting!!"
+echo "    * pull                pull latest changes from your account at github"
+echo "    * push                push latest changes to your account at github"
+echo "    * merge [githubuser]  merge with githubuser (default is ipa320)"
+}
+
 # checking input parameters
-if [ "$#" -ge 2 ]; then
-	echo "ERROR: Wrong number of parameters"
-	echo "Usage: git_helper.sh STACK"
+if [ "$#" -ge 3 ]; then
+	echo "ERROR: Wrong number of parameters, aborting..."
+	show_usage
 	exit 1
 elif [ "$1" = "status" ]; then
 	COMMAND="git status -uno"
@@ -16,30 +26,55 @@ elif [ "$1" = "pull" ]; then
 	COMMAND="git pull origin master"
 elif [ "$1" = "push" ]; then
 	COMMAND="git pull origin master && git push origin master"
-elif [ "$1" = "push320" ]; then
-	COMMAND="git checkout review-320 && git pull origin-320 master && git push origin-320 master && git checkout master"
-elif [ "$1" = "merge320" ]; then
-	COMMAND="git checkout master && git pull origin master && git checkout review-320 && git pull origin-320 master && git checkout master && git merge review-320 && git push origin master"
+elif [ "$1" = "merge" ]; then
+	if [ "$#" -ge 3 ]; then
+		echo "merging with $GITHUBUSER"
+		GITHUBUSER=$2
+	else
+		echo "merging with default ipa320"
+		GITHUBUSER="ipa320"
+	fi
 elif [ "$1" = "mergeto320" ]; then
-	COMMAND="git checkout master && git pull origin master && git checkout review-320 && git pull origin-320 master && git merge master && git push origin-320 master && git checkout master"
+	echo "merging to ipa320"
+	GITHUBUSER="ipa320"
 else
-	echo "ERROR: command <<"$1">> not supported, choose one of"
-	echo "    * status"
-	echo "    * master"
-	echo "    * commit"
-	echo "    * pull"
-	echo "    * push"
-	echo "    * push320"
-	echo "    * merge320"
-	echo "    * mergeto320"
-	echo "aborting..."
+	echo "ERROR: command <<"$1">> not supported, aborting..."
+	show_usage
 	exit 1
 fi
 
-echo "-------------------------------------------"
-echo "Executing <<$COMMAND>>"
-echo "-------------------------------------------"
-echo ""
+merge(){
+	STACK="$1"
+	
+	# check for local changes
+	COMMAND="git status --porcelain -uno"
+	cd ~/git/care-o-bot/$STACK ; status=`sh -c "$COMMAND"`
+	if [ ! -z "$status" ]; then
+		echo "ERROR: found local changes: please commit them first before merging with $GITHUBUSER, aborting..."
+		exit 1
+	fi
+
+	# push user's master
+	COMMAND="git checkout master && git pull origin master && git push origin master"
+	cd ~/git/care-o-bot/$STACK ; status=`sh -c "$COMMAND"`
+
+	# create review branch
+	COMMAND="git branch review-$GITHUBUSER -f && git checkout review-$GITHUBUSER"
+	cd ~/git/care-o-bot/$STACK ; status=`sh -c "$COMMAND"`
+
+	# create remote origin if not exists
+	COMMAND="git remote"
+	cd ~/git/care-o-bot/$STACK ; status=`sh -c "$COMMAND"`
+	echo "$status" > ~/tmp/response.txt
+	if [ $(grep -c "origin-$GITHUBUSER" ~/tmp/response.txt) == 0 ]; then
+		COMMAND="git remote add origin-$GITHUBUSER git@github.com:$GITHUBUSER/$STACK.git"
+		cd ~/git/care-o-bot/$STACK ; status=`sh -c "$COMMAND"`
+	fi
+
+	# pull from origin-$GITHUBUSER and merge and push
+	COMMAND="git pull origin-$GITHUBUSER master && git checkout master && git merge review-$GITHUBUSER && git push origin master"
+	cd ~/git/care-o-bot/$STACK ; status=`sh -c "$COMMAND"`
+}
 
 dirs=`ls ~/git/care-o-bot`
 mkdir -p ~/tmp
@@ -47,7 +82,18 @@ echo "$dirs" >  ~/tmp/stacks.txt
 while read directory
 do
 	if [ -d ~/git/care-o-bot/$directory ]; then
-		echo "==> operating on $directory"
-		cd ~/git/care-o-bot/$directory ; sh -c "$COMMAND"
+		echo "-------------------------------------------"
+		echo "==> operating on ~/git/care-o-bot/$directory"	
+		echo "-------------------------------------------"
+		
+		if [ "$1" = "merge" ]; then
+			merge $directory
+		elif [ "$1" = "mergeto320" ]; then
+			merge $directory
+			COMMAND="git checkout review-ipa320 && git merge master && git push origin-ipa320 master && git checkout master"
+		else
+			cd ~/git/care-o-bot/$directory ; sh -c "$COMMAND"
+		fi
 	fi
+	echo ""
 done < ~/tmp/stacks.txt

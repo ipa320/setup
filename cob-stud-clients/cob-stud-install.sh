aa0960eaf2ee80a40962fc91c2c2f3717e5b9ad1
stud-admin <stud-admin@stud-admin-desktop.(none)>
2011-09-19 15:41:46 +0200

    added cob-stud scripts

---
+++ cob-stud-clients/cob-stud-install.sh
@@ -0,0 +1,35 @@
+#!/usr/bin/env bash
+
+# Script for installing apt packages on all cob-stud-pc's.
+
+# checking input parameters
+if [ "$#" -lt 1 ]; then
+	echo "ERROR: Wrong number of parameters"
+	echo "Usage: $0 packages"
+	exit 1
+fi
+
+client_list="
+cob-stud-1
+cob-stud-2
+cob-stud-3
+cob-stud-4
+cob-stud-5
+cob-stud-6
+cob-stud-7
+cob-stud-8
+cob-stud-9"
+
+for client in $client_list; do
+	echo "-------------------------------------------"
+	echo "Installing <<"$*">> on $client"
+	echo "-------------------------------------------"
+	echo ""
+	ssh $client "sudo apt-get install $* -y"
+	ret=${PIPESTATUS[0]}
+	if [ $ret != 0 ] ; then
+		echo "apt-get return an error (error code: $ret), aborting..."
+		exit 1
+	fi
+	echo ""
+done

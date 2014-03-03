a033b16978bc555fd3a9c5a25a7c962d98c48e48
ipa-mig <Matthias.Gruhler@ipa.fraunhofer.de>
2014-03-03 14:32:11 +0100

    remove typo from copy'n'paste

--- templates/license_header_ipa.cpp
+++ templates/license_header_ipa.cpp
@@ -7,7 +7,7 @@
  *   Fraunhofer Institute for Manufacturing Engineering
  *   and Automation (IPA) \n
  *
- *   All rightes reserved. (IPA) \n\n
+ *   All rightes reserved. \n\n
  *
  *****************************************************************
  *

6c5883d5bf45695e614d263977d53ae775415c4f
ipa-mig <Matthias.Gruhler@ipa.fraunhofer.de>
2014-03-03 14:00:31 +0100

    add IPA only license headers

---
+++ templates/license_header_ipa.py
@@ -0,0 +1,29 @@
+#!/usr/bin/python
+#################################################################
+##\file
+#
+# \note
+#   Copyright (c) TODO FILL IN YEAR HERE \n
+#   Fraunhofer Institute for Manufacturing Engineering
+#   and Automation (IPA) \n
+#
+#   All rights reserved. \n\n
+#
+#################################################################
+#
+# \note
+#   Repository name: TODO FILL IN REPOSITORY NAME HERE
+# \note
+#   ROS package name: TODO FILL IN PACKAGE NAME HERE
+#
+# \author
+#   Author: TODO FILL IN AUTHOR NAME HERE
+# \author
+#   Supervised by: TODO FILL IN CO-AUTHOR NAME(S) HERE
+#
+# \date Date of creation: TODO FILL IN DATE HERE
+#
+# \brief
+#   TODO FILL IN BRIEF DESCRIPTION HERE
+#
+#################################################################

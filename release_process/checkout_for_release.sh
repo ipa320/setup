98baae75af70a47599d10333268a892ea6949117
Felix Messmer <felix.messmer@ipa.fhg.de>
2016-04-02 09:02:49 +0200

    fix fetch

--- release_process/checkout_for_release.sh
+++ release_process/checkout_for_release.sh
@@ -12,7 +12,7 @@ echo "doing release for repo: $repo"
 
 git clone git@github.com:ipa320/$repo -b indigo_dev
 cd $repo
-git fetch origin -p --all
+git fetch -p --all
 git checkout -b indigo_release_candidate origin/indigo_release_candidate
 git merge indigo_dev
 git push origin indigo_release_candidate

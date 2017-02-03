f4caee693b332b97e9cde288839828a6b0db5598
ipa-mig <Matthias.Gruhler@ipa.fraunhofer.de>
2017-02-03 11:03:59 +0100

    update license templates

--- templates/license_header_ipa.cpp
+++ templates/license_header_ipa.cpp
@@ -1,29 +1,6 @@
-/*!
- *****************************************************************
- * \file
- *
- * \note
- *   Copyright (c) TODO FILL IN YEAR HERE \n
- *   Fraunhofer Institute for Manufacturing Engineering
- *   and Automation (IPA) \n
- *
- *   All rights reserved. \n\n
- *
- *****************************************************************
- *
- * \note
- *   Repository name: TODO FILL IN REPOSITORY NAME HERE
- * \note
- *   ROS package name: TODO FILL IN PACKAGE NAME HERE
- *
- * \author
- *   Author: TODO FILL IN AUTHOR NAME HERE
- * \author
- *   Supervised by: TODO FILL IN CO-AUTHOR NAME(S) HERE
- *
- * \date Date of creation: TODO FILL IN DATE HERE
- *
- * \brief
- *   TODO FILL IN BRIEF DESCRIPTION HERE
- *
- ****************************************************************/
+/*
+ * Copyright (c) <TODO YEAR> Fraunhofer Institute for Manufacturing Engineering and Automation (IPA)
+ * All rights reserved.
+ * Unauthorized copying of this file, via any medium is strictly prohibited.
+ * Proprietary and confidential.
+ */
